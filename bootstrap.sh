65bd68f03c3c8f0477f8c9947eaa7a50bee8fff9
Toni Miettinen <sessio@gmail.com>
2014-08-13 18:03:27 +0300

    New shiny installer

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,33 @@
 #!/bin/bash
 
+MARKER="sessio.dotfiles-generated"
+DOTSCRIPT="$HOME/.bashrc.dotfiles"
+
 # sync this dir to home
-rsync -av --no-perms --exclude .git --exclude bootstrap.sh --exclude scripts --exclude README.md --exclude global-conf . ~
+echo "> Syncing dotfiles to $HOME"
+rsync -q -av --no-perms --exclude .git --exclude bootstrap.sh --exclude scripts --exclude README.md --exclude global-conf . ~
 
 # install Vundle
-mkdir -p ~/.vim/bundle
-git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+echo "> Install Vundle"
+if [ -d "$HOME/.vim/bundle/Vundle.vim" ]; then
+    echo "  ! skip, already installed"
+else
+    mkdir -p ~/.vim/bundle
+    git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
+
+echo "> Install Vim plugins (Vim may cry for .vimrc errors at this point, dont mind)"
 vim +PluginInstall +qall
 
-source ~/.bashrc
+echo "> Append custom bash script to .profile"
+if grep -q "$MARKER" ~/.profile; then
+    echo "  ! skip, marker already present"
+else
+    echo "if [ -f \"$DOTSCRIPT\" ]; then source $DOTSCRIPT; fí # ${MARKER}" >> ~/.profile
+fi
+
+echo "> Sourcing .bashrc.dotfiles"
+source ~/.bashrc.dotfiles
+
+echo
+echo "> Done, enjoy!"
